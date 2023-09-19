44b9a26cfbca50737278d089d93aafe661c86882
neutronahmed <bazubairu@st.ug.edu.gh>
2023-09-19 21:41:50 +0000

    fix: stock graphs and data streaming

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
 
@@ -33,7 +33,17 @@ class Graph extends Component<IProps, {}> {
   componentDidMount() {
     // Get element to attach the table from the DOM.
     const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    elem.setAttribute('view', 'y_line')
+    elem.setAttribute('column-pivots', '["stock"]')
+    elem.setAttribute('row-pivots','["timestamp"]')
+    elem.setAttribute('columns','["top_ask_price"]')
+    elem.setAttribute('aggregrates', `
+    {"stock": "distinct count"
+    top_ask_price: "avg"
+    top_bid_price: "avg"
+    "timestamp": "distinct count"
+    }
+    `)
     const schema = {
       stock: 'string',
       top_ask_price: 'float',
@@ -61,8 +71,8 @@ class Graph extends Component<IProps, {}> {
         // Format the data from ServerRespond to the schema
         return {
           stock: el.stock,
-          top_ask_price: el.top_ask && el.top_ask.price || 0,
-          top_bid_price: el.top_bid && el.top_bid.price || 0,
+          top_ask_price: (el.top_ask && el.top_ask.price) || 0,
+          top_bid_price: (el.top_bid && el.top_bid.price) || 0,
           timestamp: el.timestamp,
         };
       }));
